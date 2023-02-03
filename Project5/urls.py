944636bad249a34e23037e4d32f4dc2c38319048
sriram62 <v0500771@gmail.com>
2023-02-03 13:18:14 +0530

    Initial commit

---
+++ Project5/urls.py
@@ -0,0 +1,29 @@
+"""Rajkumarproject5 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from modelapp.views import HomeView,InsertInput,InsertView,DisplayView,DeleteInputView,DeleteView,UpdateInputView,UpdateView
+urlpatterns = [
+    path('admin/',admin.site.urls),
+    path('modelapp/',HomeView.as_view()),
+    path('modelapp/insertinput/',InsertInput.as_view()),
+    path('modelapp/insertinput/insert/',InsertView.as_view()),
+    path('modelapp/display/',DisplayView.as_view()),
+    path('modelapp/deleteinput/',DeleteInputView.as_view()),
+    path('modelapp/deleteinput/delete/',DeleteView.as_view()),
+    path('modelapp/updateinput/',UpdateInputView.as_view()),
+    path('modelapp/updateinput/update/',UpdateView.as_view()),
+]
---
+++ modelapp/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Product (models.Model):
+ pid=models.IntegerField(primary_key=True)
+ pname=models.CharField(max_length=50)
+ pcost=models.DecimalField(max_digits=10,decimal_places=2)
+ pmfdt=models.DateField()
+ pexpdt=models.DateField()
